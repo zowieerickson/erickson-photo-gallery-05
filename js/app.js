6deeb2330314e88da4a67c20995a418e7edd9b4c
Zowie Erickson <zowie.erickson@gmail.com>
2021-03-15 12:55:28 -0400

    added JS to target input value & target image captions

--- js/app.js
+++ js/app.js
@@ -1,2 +1,33 @@
 // import baguetteBox from "baguettebox.js";
 baguetteBox.run(".gallery");
+const input = document.getElementById("search");
+const filter = input.value.toUpperCase();
+const ul = document.getElementsByClassName("gallery");
+const li = document.getElementsByClassName("item");
+
+// const imgCaption = document.getElementsByTagName("a")[0];
+// imgCaption.getAttribute("data-caption");
+
+// console.log(imgCaption);
+
+(function filterImages() {
+  for (i = 0; i <= 12; i++) {
+    let imgCaption = document
+      .getElementsByTagName("a")
+      [i].getAttribute("data-caption");
+    imgCaption = imgCaption.toLowerCase();
+    console.log(imgCaption);
+  }
+})();
+
+/*
+input.addEventListener("keyup", lowerInput);
+
+function lowerInput() {
+  input.value = input.value.toLowerCase();
+  console.log(input.value);
+  if (input.value.includes(imgCaption)) {
+    console.log(imgCaption);
+  }
+}
+*/
